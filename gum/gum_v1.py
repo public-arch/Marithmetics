453f23df57fc7684dd68e15de1abeca49893a6d6
public-arch <public@marithmetics.com>
2025-12-02 21:48:18 -0500

    Add files via upload

--- gum/gum_v1.py
+++ gum/gum_v1.py
@@ -38,6 +38,7 @@ from sm import sm_math_model_demo_v1 as smath
 from sm import sm_standard_model_demo_v1 as sm33
 from cosmo import bb_grand_emergence_masterpiece_runner_v1 as bb36
 
+
 # ---------------------------------------------------------------------------
 # Data containers
 # ---------------------------------------------------------------------------
